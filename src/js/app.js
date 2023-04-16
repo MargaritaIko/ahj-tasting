b2e5eb88dff99a162aca83073978ac837cbcfcc9
Margarita May Lee <ritochka.m_lee@yahoo.com>
2023-04-16 21:34:51 +0300

    Fixed Ver

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-
+import image from "../img/goblin.png";
 
 export default class goblinGame {
   constructor() {
@@ -18,21 +18,21 @@ export default class goblinGame {
 
   boardGeneration(countOfCells) {
     this.boarSize = countOfCells;
-    this.field = document.createElement('div');
-    this.field.classList.add('board');
+    this.field = document.createElement("div");
+    this.field.classList.add("board");
     const width = 122 * countOfCells;
     this.field.style.width = `${width}px`;
     document.body.appendChild(this.field);
     for (let i = 0; i < countOfCells ** 2; i += 1) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
       this.field.appendChild(cell);
     }
   }
 
   imgCreate(interval) {
-    const goblin = document.createElement('img');
-    goblin.setAttribute('src', '../img/goblin.png');
+    const goblin = new Image();
+    goblin.src = image;
     setInterval(() => {
       const position = this.goblinSetter();
       this.field.childNodes[position].appendChild(goblin);
